c64e79c5ec551bdea7a06d95ad8eca7eb10e240b
Diego Ignacio Rojas Gonzalez <gonzalez.rojas.ignacio.diego@gmail.com>
2025-12-01 02:13:09 -0300

    fix: update installation method to use gradlew instead of gradle

--- mish.rb
+++ mish.rb
@@ -2,19 +2,22 @@ class Mish < Formula
   desc "Streamlined Android development tool - One-command app deployment"
   homepage "https://github.com/astralcollar/mish-cli"
   url "https://github.com/astralcollar/mish-cli/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "a1aac582560a1483001978b812f7efdbba6167484efca038c7db72ad19a885b1" 
+  sha256 "a1aac582560a1483001978b812f7efdbba6167484efca038c7db72ad19a885b1"
   license "MIT"
 
   depends_on "openjdk@11"
-  depends_on "gradle"
 
   def install
-    system "gradle", "installDist"
+    ENV["JAVA_HOME"] = Formula["openjdk@11"].opt_prefix
+    
+    # Use gradlew wrapper included in the project
+    system "./gradlew", "installDist"
+    
     libexec.install Dir["build/install/mish-cli/*"]
     bin.install_symlink "#{libexec}/bin/mish-cli" => "mish"
   end
 
   test do
-    system "#{bin}/mish", "--help"
+    system bin/"mish", "--help"
   end
 end
