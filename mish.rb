f77f982cb6897aa4e2e7cd16fd0b22a082cf0c2d
Diego Ignacio Rojas Gonzalez <gonzalez.rojas.ignacio.diego@gmail.com>
2025-12-01 02:06:50 -0300

    update sha

--- mish.rb
+++ mish.rb
@@ -2,7 +2,7 @@ class Mish < Formula
   desc "Streamlined Android development tool - One-command app deployment"
   homepage "https://github.com/astralcollar/mish-cli"
   url "https://github.com/astralcollar/mish-cli/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "PENDING_SHA256" 
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed" 
   license "MIT"
 
   depends_on "openjdk@11"
